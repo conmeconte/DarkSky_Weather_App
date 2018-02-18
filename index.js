35ab3a46dd3d52c87e2f99941e604dad281c2fd3
conmeconte <saja523@hotmail.com>
2018-02-17 21:49:48 -0800

    trying different port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const DarkSky = require('dark-sky')
 const darksky = new DarkSky(keys.API_KEY); 
 
 const app   = express();
-const PORT  = process.env.PORT || 8080;
+const PORT  = process.env.PORT || 8000;
 
 
 app.use(express.json());
