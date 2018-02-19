492897a3e4530e1a9677a51127afb23a819d85b5
conmeconte <saja523@hotmail.com>
2018-02-18 16:36:01 -0800

    change bg on mobile

--- client/src/components/date_search_bar.js
+++ client/src/components/date_search_bar.js
@@ -58,7 +58,7 @@ class DateSearchBar extends Component {
         return (
             <div className="form_container col-12">
                 <div className="video">
-                    <iframe src="https://www.youtube.com/embed/iGpuQ0ioPrM?controls=0&showinfo=0&playlist=iGpuQ0ioPrM&autoplay=1&loop=1" frameBorder="0" allowFullScreen></iframe>   
+                    <iframe src={/Mobi/.test(navigator.userAgent) ?'../assets/images/hero.JPG' :"https://www.youtube.com/embed/iGpuQ0ioPrM?controls=0&showinfo=0&playlist=iGpuQ0ioPrM&autoplay=1&loop=1"} frameBorder="0"></iframe>   
                 </div>
                 <form onSubmit={this.props.handleSubmit(this.onFormSubmit.bind(this))}>
                     <h1 className="text-center pt-3">Last Week's Weather Information</h1>
