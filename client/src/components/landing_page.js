981404ad216fa1b119e8d036fc9a23426f0bbff7
conmeconte <saja523@hotmail.com>
2018-02-18 18:08:04 -0800

    added bottom margin of form

--- client/src/components/landing_page.js
+++ client/src/components/landing_page.js
@@ -22,7 +22,7 @@ export default ()=>{
                             <Link className="dropdown-item" to="/search">Last Week</Link>
                         </div>
                     </div>
-                    <div className="mobile">
+                    <div className="mobile my-5">
                         <Link className="btn btn-outline-info mr-3" to="/search/date">By Date</Link>
                         <Link className="btn btn-outline-info mr-3" to="/search">Last Week</Link>
                     </div>
