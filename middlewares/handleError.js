53d200d4cd91730ffcde500510b09adf3e15d207
conmeconte <saja523@hotmail.com>
2018-02-21 12:01:06 -0800

    node error handler connected

--- middlewares/handleError.js
+++ middlewares/handleError.js
@@ -1,21 +1,6 @@
 const fs            = require('fs');
 const path = require('path');
-exports.logError = function (err, req, res, next){
-    if (!err) {
-        next()
-    } else {
-        console.log('Error will be logged')
-        let errorData= {Date: new Date().toLocaleString(),errorMessage: err.stack};
-        fs.appendFile(path.join(__dirname, '..', 'errorLogs', 'serverError.log'), JSON.stringify(errorData) + '\n', function (err) {
-            if (err) next(err); 
-            console.log('Updated!');
-         });
-        res.status(500).render('error', { error: err });
-        next(err);
-    }
 
-
-};
 
 exports.errorHandler = (err, req, res, next)=>{
     if (res.headersSent) {
@@ -25,12 +10,3 @@ exports.errorHandler = (err, req, res, next)=>{
       res.render('error', { error: err })
     
 };
-
-exports.clientErrorHandler=(err, req, res, next)=>{
-    if (req.xhr) {
-        res.status(500).send({ error: 'Something failed!' })
-      } else {
-        next(err)
-      }
-    
-}
