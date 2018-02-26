aa61bebcd19b2785139d9c86939b7312d0c1a65b
conmeconte <saja523@hotmail.com>
2018-02-26 11:16:46 -0800

    a

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,28 +2,32 @@ var moment = require('moment');
 
 module.exports = (app, darksky)=>{
 
-    app.post('/api/weather', (req, res, next)=>{
-        console.log('api time', req.body.date); 
-        darksky
-        .options({
-            latitude: req.body.lnglat.lat,
-            longitude: req.body.lnglat.lng,
-            time: req.body.date || moment().subtract(1, 'weeks'),
-            language: 'en',
-            exclude: ['minutely'],
-            extendHourly: true
+        app.post('/api/weather', (req, res, next)=>{
+                darksky
+                .options({
+                    latitude: req.body.lnglat.lat,
+                    longitude: req.body.lnglat.lng,
+                    time: req.body.date || [moment().subtract(1, 'weeks')],
+                    language: 'en',
+                    exclude: ['minutely'],
+                    extendHourly: true
+                })
+                .get((err)=>{
+                    if(err){
+                        return next(err); 
+                    }
+                })
+                .then(value=>{
+                    res.send(value); 
+
+                })
+                .catch(err=>{
+                    res.send(err); 
+                })
         })
-        .get((err)=>{
-            if(err){
-                return next(err); 
-            }
-        })
-        .then(value=>{
-            res.send(value); 
-        })
-        .catch(err=>{
-            res.send(err); 
-        })
-    })
+        
+
+    
+
 
 }
