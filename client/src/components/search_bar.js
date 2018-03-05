d27938fac0908436fe538ad492a8415b300541d2
conmeconte <32534202+conmeconte@users.noreply.github.com>
2018-03-04 23:54:14 -0800

    trying to make it faster

--- client/src/components/search_bar.js
+++ client/src/components/search_bar.js
@@ -48,14 +48,16 @@ class SearchBar extends Component {
         
 
         this.props.reset(); 
-
-        // axios.get(URL).then((data)=>{
+        //Faster but inconsistent date order
+        // axios.get(URL).then(async (data)=>{
         //     for(let i=7; i>=1; i--){
-        //         let pastWeekDay= moment().subtract(i, 'days')._d;
-        //         sendingData['date']= pastWeekDay;
-        //         lnglat= data.data.results[0].geometry.location;
-        //         sendingData["lnglat"]= lnglat
-        //         this.props.fetchWeather(sendingData); 
+        //         // (()=>{
+        //             let pastWeekDay= await moment().subtract(i, 'days')._d;
+        //             sendingData['date']= pastWeekDay;
+        //             lnglat= data.data.results[0].geometry.location;
+        //             sendingData["lnglat"]= lnglat
+        //             this.props.fetchWeather(sendingData); 
+        //         // })(); 
         //     }
 
         // });
